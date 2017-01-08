6753c6b49545b1397981e3cec371a840420b1df5
Nicolas Sam <Nicolas Sam>
2017-01-08 04:38:21 -0500

    Added sticky event headers

--- static-root/js/components/autosearch.js
+++ static-root/js/components/autosearch.js
@@ -34,7 +34,6 @@
             return neatResults;
         });
         var sortedData = cleanedData.sort(function(a,b){
-            console.log(a.name,b.name);
             return a.name.toUpperCase() > b.name.toUpperCase();
         });
 
